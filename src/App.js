b308cc897091eb599cebfaa28d8fccaae7678770
Eduardo <duoeduardo@gmail.com>
2020-11-23 19:16:02 -0300

    agrega tempalte basic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
-
-
+import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Salu2");
+  };
+
   return (
-<h1>Hola</h1>
+    <>
+      <section className="container">
+        <h3>Generador de colores</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn" type="submit">
+            Buscar
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>Lista aqui</h4>
+      </section>
+    </>
   );
 }
 
-export default App
+export default App;
